d17925c71faa9435b1393e2a73591f689ba7992a
w3cmark <lijungm305@gmail.com>
2015-08-16 21:14:14 +0800

    更新

--- kuaiyi/src/src/data/index.js
+++ kuaiyi/src/src/data/index.js
@@ -1,10 +1,66 @@
 var Index = function(){
-	var init = function(){
+	var $regionpop = $('#Jregionpop'),
+		$hospitalpop = $('#Jhospitalpop'),
+		$departmentpop = $('#Jdepartmentpop'),
+		$doctorpop = $('#Jdoctorpop'),
+		$closepop = $('.close-pop'),
+		$inputpop = $('.banner-right .input-pop'),
+		$datepop = $('#Jdatepop'),
+	init = function(){
 		fixPlaceholder();
 		qudaoFun();
-		//调用
-		selectMod('#Jselect','#Joption',true);
-		fixHeader();
+		//地区弹层
+		$('#Jregion').bind('click',function(){
+			if($regionpop.hasClass('show')){
+				hidePop($regionpop);
+			}else{
+				showPop($regionpop);
+				hidePop($hospitalpop);
+				hidePop($departmentpop);
+				hidePop($doctorpop);
+			}
+		})
+		$closepop.bind('click',function(){
+			hidePop($(this).parent().parent());
+		})
+
+		$inputpop.bind('focus',function(){
+			showPop($(this).parent().parent().find('.select-pop'));
+		})
+
+		// 医生弹层滚动条
+		$("#Jdoctorpop .pop-tb").niceScroll({
+            cursorcolor: "#e5e5e5",
+            cursorwidth: "10px",
+            cursorborder: "0",
+            cursorborderradius: "10px",
+            scrollspeed:80,
+            autohidemode:true
+        });
+        // 日期弹层
+        $('#Jdate').bind('click',function(){
+			if($datepop.hasClass('show')){
+				hidePop($datepop);
+			}else{
+				showPop($datepop);
+			}
+		})
+
+		//点击页面其它地方关闭弹层
+		$(document).bind('click',function(e){
+			var target = $(e.target);
+			if(target.attr('id') !='Jregion' && !target.hasClass('input-pop') && target.closest(".select-pop").length == 0){
+				hidePop($regionpop);
+				hidePop($hospitalpop);
+				hidePop($departmentpop);
+				hidePop($doctorpop);
+			}
+			if(target.closest("#Jdate").length == 0 && target.closest("#Jdatepop").length == 0){
+				hidePop($datepop);
+			}
+		})
+
+		fixHeader();		
 	},
 	fixPlaceholder = function(){//兼容输入框placeholder
 	    $('input[placeholder], textarea[placeholder]').focus(function(){
@@ -148,3 +204,5 @@ Index.booklistFun();
 // Index.showPop($('#Jdatepop'));
 //隐藏某个弹层
 // Index.hidePop($('#Jdatepop'));
+
+// Index.showPop($('#Jhospitalpop'));

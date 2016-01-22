81be677cffc4dfb5763d92fedda1ab1165088f53
grp.wzj <grp.wzj@corp.netease.com>
2016-01-22 13:33:14 +0800

    修改打包配置

--- kuaiyi/src/fis-conf.js
+++ kuaiyi/src/fis-conf.js
@@ -1,149 +1,186 @@
+
 <!--param start-->
     //修改cdn的绝对路径（测试环境）
-    fis.config.set('cdn-path','http://guahao.mingyihui.net/templates/default');
+    fis.set('cdn-path','.');
     //修改cdn的绝对路径（正式环境）
-    fis.config.set('cdn-path-release','http://guahao.mingyihui.net/templates/default');
+    fis.set('cdn-path-release','.');
     //修改雪碧图放大缩小倍数，默认是1，iphone是0.5
-    fis.config.set('css-scale',1);
+    fis.set('css-scale',1);
     //修改include文件的域名
-    fis.config.set('include-host','http://zgmh.163.com')
-    //修改打包配置
-    fis.config.set('pack', {
-        'pkg/lib.js': [
-            'src/js/lib/**.js'
-        ]
-    });
+    fis.set('include-host','http://kf.163.com')
 <!--end-->
 
-fis.config.set('project.charset', 'utf-8');
-fis.config.set('project.include', 'src/**');
+//配置通用
+fis.set('project.files', ['src/**']);
+fis.set('project.ignore', ['node_modules/**', 'dist/**', 'release/**', 'README.md' , 'local/**' ,'.git/**', 'fis-conf.js']);
+fis.set('charset', 'utf-8');
+fis.set('project.charset', 'utf-8');
 
-fis.config.merge({
-    modules : {
-        parser : {
-            less : 'less',
-            tmpl : 'bdtmpl',
-            html : 'html-ejs'
-        },
-        postpackager : 'simple,supply',
-        postprocessor :{
-            html : 'include',
-            js : 'replace'
-        },
-        spriter : 'csssprites'
-    },
-    settings : {
-        postpackager : {
-            simple : {
-                autoCombine : true
-            }
-        },
-        postprocessor : {
-            include : {
-                host : fis.config.get('include-host'),
-                debug : true,
-                release : false,
-                encode : 'gbk'
-            },
-            replace : {
-                local_cdn : "http://127.0.0.1:8080",
-                debug_cdn : fis.config.get("cdn-path"),
-                release_cdn : fis.config.get("cdn-path-release")
-            }
-        },
-        spriter:  {
-            csssprites : {
-                margin: 10,
-                //图片缩放比例，iphone为0.5，默认1
-                scale: fis.config.get('css-scale'),
-                layout: 'matrix'
-            }
-        }
-    },
-    roadmap : {
-        ext: {
-            less: 'css'
-        },
-        domain : fis.config.get('cdn-path'),
-        path : [
-            {
-                reg : /^\/src\/.*\/(_.*)$/i,
-                release : 'include/$1'
-            },
-            {
-                reg : /^\/src\/inline\/(.*)$/i,
-                release : false
-            },
-            {
-                reg : /^\/src\/(.*\.html)$/i,
-                useCache : false,
-                release : '$1'
-            },
-            {
-                reg : /^\/src\/js\/(.*\.js)$/i,
-                release : 'js/$1'
-            },
-            {
-                reg : /^\/src\/css\/(.*\.less)$/i,
-                release : 'css/$1',
-                useSprite: true,
-                useHash : false
-            },
-            {
-                reg : /^\/src\/css\/(.*\.css)$/i,
-                release : 'css/$1',
-                useSprite: true,
-                useHash : false
-            },
-            {
-                reg : /^\/src\/css\/(.*\.png)$/i,
-                release : 'img/spriter/$1',
-                useSprite: true
-            },
-            {
-                reg : /^\/src\/img\/(.*)$/i,
-                release : 'img/$1',
-                useHash : false
-            },
-            {
-                reg : /^\/src\/data\/(.*)$/i,
-                release : 'data/$1',
-                useHash : false,
-                useDomain : true,
-                useSprite : true
-            },
-            {
-                reg : '**.tmpl',
-                isJsLike : true,
-                release : false
-            },
-            {
-                reg : '**.js',
-                release : '$&'
-            },
-            {
-                reg : '**.css',
-                release : '$&',
-                useSprite: true
-            },
-            {
-                reg : '**',
-                release : false
-            }
-        ]
-    },
-    deploy : {
-        dist : {
-            to : './dist',
-            exclude : /\/js\/|\/include\//i,
-        },
-        release : {
-            to : './release',
-            exclude : /\/js\/|\/include\//i,
-            replace : {
-                from : fis.config.get('cdn-path'),
-                to : fis.config.get('cdn-path-release')
-            }
-        }
-    }
+fis.match('**.less', {
+    parser: fis.plugin('less'), // invoke `fis-parser-less`,
+    rExt: '.css'
 });
+fis.match('**.tmpl', {
+    parser: fis.plugin('bdtmpl'),// invoke `fis-parser-bdtmpl`
+    isJsLike : true,
+    release : false
+},true);
+fis.match('**.html', {
+    parser: fis.plugin('html-ejs'), // invoke `fis-parser-html-ejs`
+    postprocessor : fis.plugin('include',{
+        host : fis.get('include-host'),
+        debug : true,
+        release : false,
+        encode : 'utf-8',
+    })
+});
+fis.match('**.js', {
+    preprocessor: fis.plugin('resload'),
+    postprocessor : fis.plugin('replace',{
+        local_cdn : "http://127.0.0.1:8080",
+        debug_cdn : fis.get("cdn-path"),
+        release_cdn : fis.get("cdn-path-release"),
+        debug : "local"
+    })
+});
+
+fis.match(/^\/src\/(.*)$/i,{
+    release : "$1",
+    useCache : false
+});
+fis.match(/^\/src\/css\/_.*\.(css|less)/i,{
+    release : false
+});
+
+fis.match(/^\/src\/.*\/(_.*)$/i,{
+    release : "temp_file/$1"
+});
+
+fis.match(/^\/src\/css\/(.*\.png)$/i,{
+    release : "img/spriter/$1"
+});
+
+// fis.match(/^\/src\/data\/(.*)$/i,{
+//     useHash : false,
+//     useDomain : true,
+//     useSprite : true
+// },true);
+//
+//fis.match('src/js/lib/*.js',{
+//  packTo : 'pkg/lib.js'
+//});
+
+//配置打本地包
+
+fis.hook('relative');
+fis.media('local')
+    .match('::package', {
+        spriter: fis.plugin('csssprites',{
+            layout: 'matrix',
+            margin: '5px'
+        })
+    })
+    .match('*.{css,less}',{
+        useSprite : true
+    })
+    // .match(/^\/src\/data\/(.*)$/i,{
+    //     useHash : false,
+    //     useDomain : false,
+    //     useSprite : false
+    // },true)
+    .match('**', {
+      relative: true,
+      useDomain : false,
+      charset : fis.get("charset"),
+      deploy: fis.plugin('encoding')
+    })
+    .match("**.html",{
+        postprocessor : fis.plugin('include',{
+            nouse : true
+        })
+    })
+    .match('**', {
+      deploy: fis.plugin('local-supply', {
+          to: './local',
+          exclude : ['inline','temp_file']
+      })
+    })
+
+//配置测试打包
+
+// fis.media('dist')
+//     .match('**.js', {
+//         postprocessor : fis.plugin('replace',{
+//             debug : "dist"
+//         })
+//     })
+//     .match('*.{js,css,less,png,jpg,jpeg,gif,mp3,mp4,flv,swf,svg,eot,ttf,woff}',{
+//         domain: fis.get("cdn-path"),
+//         useHash: true
+//     })
+//     .match('::package', {
+//         spriter: fis.plugin('csssprites',{
+//             layout: 'matrix',
+//             margin: '5px'
+//         }),
+//         postpackager : [fis.plugin('usemin'),fis.plugin('supply')]
+//     })
+//     .match('*.{css,less}',{
+//         useSprite : true
+//     })
+//     .match('**', {
+//         charset : fis.get("charset"),
+//         deploy: [fis.plugin('encoding'),fis.plugin('local-supply', {
+//             to: './dist',
+//             exclude : ['inline','temp_file']
+//         })]
+//     })
+
+// //配置正式打包
+
+// fis.media('release')
+//     .match('**.js',{
+//         postprocessor : fis.plugin('replace',{
+//             debug : "release"
+//         }),
+//         optimizer: fis.plugin('uglify-js',{
+//             output : {
+//                 ascii_only : true
+//             }
+//         })
+//     })
+//     .match('**.html:js',{
+//         optimizer: fis.plugin('uglify-js')
+//     })
+//     .match('*.{css,less}',{
+//         optimizer: fis.plugin('clean-css')
+//     })
+//     .match('**html:css',{
+//         optimizer: fis.plugin('clean-css')
+//     })
+//     .match("**.html",{
+//         postprocessor : fis.plugin('include',{
+//             nouse : true
+//         })
+//     })
+//     .match('*.{js,css,less,png,jpg,jpeg,gif,mp3,mp4,flv,swf,svg,eot,ttf,woff}',{
+//         domain: fis.get("cdn-path-release"),
+//         useHash: true
+//     })
+//     .match('::package', {
+//       spriter: fis.plugin('csssprites',{
+//             layout: 'matrix',
+//             margin: '5px'
+//         }),
+//         postpackager : [fis.plugin('usemin'),fis.plugin('supply')]
+//     })
+//     .match('*.{css,less}',{
+//         useSprite : true
+//     })
+//     .match('**', {
+//         charset : fis.get("charset"),
+//         deploy: [fis.plugin('encoding'),fis.plugin('local-supply', {
+//             to: './release',
+//             exclude : ['inline','temp_file']
+//         })]
+//     })

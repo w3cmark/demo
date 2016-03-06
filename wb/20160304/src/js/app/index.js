03266736f076e52027d0bb0ccfa5a4105c606389
grp.wzj@corp.netease.com <grp.wzj@corp.netease.com>
2016-03-06 15:56:55 +0800

    更新

--- wb/20160304/src/js/app/index.js
+++ wb/20160304/src/js/app/index.js
@@ -32,209 +32,6 @@ var Index = function(){
         _css3pre = vendor ? '-' + vendor.toLowerCase() + '-' : '',
         csstrans = {},
         _audioTimeout,
-        _messName = '',
-        _dataArr = {
-            'boy':{
-                'name':[
-                    '貂蝉',
-                    '小乔',
-                    '大乔'
-                ],
-                '貂蝉':{
-                    'icon':'s-icon-dc',
-                    'txt0':'布布，你的臂孔武有力，你的胸宽广伟岸，你就是我心目中的英雄。',
-                    'txt1':[
-                        '我就是你的吕布，跟我走吧。',
-                        '我不是吕布，你认错人了。'
-                    ],
-                    'txt2':[
-                        ['布布，我要追随你到天涯海角。',
-                        '我就知道你一定会来找我的。'],
-                        ['布布不在身边，每一天都很难熬，你能帮我找到布布吗？',
-                        '那你帮我把我的心声传达给布布吗？']
-                    ],
-                    'txt3':[
-                        '别怕，我一定会好好对你的。',
-                        '我尽力帮你去找，只是万一找不到呢？'
-                    ],
-                    'audio':[
-                        '/data/dc1.mp3',
-                        '/data/dc2.mp3'
-                    ],
-                    'audio_time':[
-                        '3',
-                        '8'
-                    ],
-                    'audio_txt':[
-                        '官人，你就从了我吧。',
-                        '此等良辰美景，官人还在等待什么？春宵一刻值千金啊'
-                    ]
-                },
-                '小乔':{
-                    'icon':'s-icon-xq',
-                    'txt0':'无论多少年，周公谨都是我心中的唯一。',
-                    'txt1':[
-                        '周公瑾有什么好？跟我走吧',
-                        '你真是个痴情的女子，愿你一切都好。'
-                    ],
-                    'txt2':[
-                        ['我与周公瑾的感情旁人不懂。',
-                        '他的好，他的笑一直铭记在我的脑海里。此生此世，永生永世无法忘怀'],
-                        ['你真好，余生那么长，一定会有一个人让你难忘。',
-                        '谢谢官人，我只是在恰当的时候遇到了合适的他而已。']
-                    ],
-                    'txt3':[
-                        '你真是个痴情的女子。',
-                        '爱笑的女孩运气都不会太差哦。'
-                    ],
-                    'audio':[
-                        '/data/xq1.mp3',
-                        '/data/xq2.mp3'
-                    ],
-                    'audio_time':[
-                        '9',
-                        '5'
-                    ],
-                    'audio_txt':[
-                        '洞房花烛，温床软语，美人在卧，官人还来不睡吗？',
-                        '你，会是我的英雄吗？'
-                    ]
-                },
-                '大乔':{
-                    'icon':'s-icon-dq',
-                    'txt0':'孙郎去后我本想一生守寡，奈何长夜漫漫，孤独寂寞冷，现诚意招亲，欢迎广大高富帅报名参与。',
-                    'txt1':[
-                        '你怎么能如此放荡不羁？我喜欢，我报名。',
-                        '世风日下，人心不古。悲哀悲哀。'
-                    ],
-                    'txt2':[
-                        ['只愿我这朵红杏攀得了你家的墙。',
-                        '待君晨起绕床来，妾将挽发笑开颜。'],
-                        ['谁规定只有男人才能三妻四妾？女人就不能寻找真爱？',
-                        '我只做我的大乔。无关风月，无关闲语。']
-                    ],
-                    'txt3':[
-                        '乔儿，真乖。',
-                        '你如果相信我的话，我愿意带你和你妹妹走。'
-                    ],
-                    'audio':[
-                        '/data/dq1.mp3',
-                        '/data/dq2.mp3'
-                    ],
-                    'audio_time':[
-                        '4',
-                        '7'
-                    ],
-                    'audio_txt':[
-                        '呵呵，就这样，我去洗澡了',
-                        '我是只温柔的小绵羊，只愿一生服侍着我的官人'
-                    ]
-                }
-            },
-            'gril':{
-                'name':[
-                    '张飞',
-                    '诸葛亮',
-                    '王司徒'
-                ],                
-                '张飞':{
-                    'icon':'s-icon-zf',
-                    'txt0':'何为大将，就是造反。谁敢与我决一死战。',
-                    'txt1':[
-                        '你这么man，家里人知道吗？',
-                        '你这么凶，一定是单身狗无疑了'
-                    ],
-                    'txt2':[
-                        ['其实我偶尔也是可以柔情似水的，姑娘么么哒~',
-                        '家父必然是知道的。'],
-                        ['这是重点吗？',
-                        '你愿意帮我渡明年的七夕劫吗？']
-                    ],
-                    'txt3':[
-                        '你说什么？我的手机有点卡，看不清你的字。',
-                        '你这么一说我突然想起来20年没过七夕了。'
-                    ],
-                    'audio':[
-                        '/data/zf1.mp3',
-                        '/data/zf2.mp3'
-                    ],
-                    'audio_time':[
-                        '6',
-                        '3'
-                    ],
-                    'audio_txt':[
-                        '你泡不到男神，钓不到马子，你就连本飞的语音都收不到，你能不能换个手机？',
-                        '姑娘，咱们七夕不见不散啊~'
-                    ]
-                },
-                '诸葛亮':{
-                    'icon':'s-icon-zg',
-                    'txt0':'我奉召饶舌，饶舌，饶舌，饶舌，舌，舌，舌....',
-                    'txt1':[
-                        '真有节奏感，你是未来的rap之星。',
-                        '乱七八糟，不知所云。'
-                    ],
-                    'txt2':[
-                        ['谢姑娘美誉，这是在下的荣幸。',
-                        '我可以教你啊，一对一教学，童叟无欺。'],
-                        ['你不懂我如王司徒般无情无义，我真的好伤心。',
-                        '孔明从不做无利益之事，你已经中了我的迷魂计。']
-                    ],
-                    'txt3':[
-                        '真的吗？',
-                        '我这么善良单纯可爱，你怎么忍心伤害我？'
-                    ],
-                    'audio':[
-                        '/data/zg1.mp3',
-                        '/data/zg2.mp3'
-                    ],
-                    'audio_time':[
-                        '6',
-                        '10'
-                    ],
-                    'audio_txt':[
-                        '（鬼畜魔性节奏感笑声）当然，哈哈哈哈哈',
-                        '在下诸葛孔明，我一不拼爹二不坑妈三不家暴，我只是个儒雅的军师。yeah~'
-                    ]
-                },
-                '王司徒':{
-                    'icon':'s-icon-wst',
-                    'txt0':'我竟从未见过如此厚颜无耻之人',
-                    'txt1':[
-                        '酸酸甜甜就是我，我就是厚颜无耻之人。',
-                        '你太讨厌了，从没有人说过我无耻。'
-                    ],
-                    'txt2':[
-                        ['是在下输了。',
-                        '我王朗才疏学浅，你让我大开眼界。'],
-                        ['姑娘莫生气，王朗并非要羞辱姑娘。',
-                        '王朗有幸成为第一人，甚是喜悦。']
-                    ],
-                    'txt3':[
-                        '我皮厚我自豪，我出门不戴遮阳帽。',
-                        '真的不想理你。really。'
-                    ],
-                    'audio':[
-                        '/data/wst1.mp3',
-                        '/data/wst2.mp3'
-                    ],
-                    'audio_time':[
-                        '10',
-                        '7'
-                    ],
-                    'audio_txt':[
-                        '你还要听多少遍，是在下输了。是在下输了。是在下...',
-                        '转告诸葛村夫，我的低调绝不是他装B的资本。'
-                    ]
-                }
-            }
-        },
-        shareInfo = {
-            title: $('#share_title').html(),//分享标题
-            desc: $('#share_desc').html(),//分享正文
-            url: window.location.href,//分享URL
-            imgurl: $('#share_pic').attr('data-src')//分享图片
-        },
     init = function(){
         initScreen();
 
@@ -242,7 +39,8 @@ var Index = function(){
             e.preventDefault();
         })
 
-        showEle($stages.eq(2));
+        showEle($stages.eq(0));
+        // playGame();
 
         setTimeout(function(){
             _messaudio.play();
@@ -306,12 +104,82 @@ var Index = function(){
             var $this = $(e);
             $this.on('touchend',function(){
                 $stages.eq(1).hide();
+                if(i == 0){
+                    playGame();
+                }else{
+                    playVideo();
+                }
                 setTimeout(function(){
                     showEle($stages.eq(2+i));
                 },20)
                 
             })
         })
+    },
+    playGame = function(){
+        var $gamestage = $('#Jgamestage'),
+            $begain_btn = $('#Jgameplay'),
+            $num = $gamestage.find('.num');
+        $begain_btn.on('touchend',function(){
+            $gamestage.addClass('play');
+            $(this).hide();
+            showEle($num);
+            //3s倒计时
+            changeTime($num.find('i'), 3,function(ele){
+                ele.text('GO!');
+                setTimeout(function(){
+                    hideEle($num);
+                    
+                    addEvent($gamestage.find('.game-btn a'));
+                    // 计时
+                    $gamestage.find('.game-time span').addClass('on');
+                    changeTime($gamestage.find('.game-time i'), 6,function(ele){
+                        ele.text('0');
+                        $num.css('width','85%');
+                        $num.find('i').text('Game Over!')
+                        showEle($num);
+                        $gamestage.find('.game-btn').hide();
+
+                        //游戏结束3s后播放视频
+                        setTimeout(function(){
+                            playVideo();
+                        },3000)
+                        
+                    })
+
+                },300)
+                showEle($gamestage.find('.game-btn'));
+                showEle($gamestage.find('.game-peo'));
+            });
+        })
+        function addEvent(ele){
+            var num = 0,
+                $hit = $gamestage.find('.game-hit'),
+                $peo = $gamestage.find('.game-peo');
+            ele.on('touchstart',function(){
+                _messaudio.pause();
+                _messaudio.play();
+                $(this).addClass('on');
+                $peo.addClass('on');
+            })
+            ele.on('touchend',function(){
+                $(this).removeClass('on');
+                $peo.removeClass('on');
+                num++;
+                $hit.text(num);
+            })
+        }
+    },
+    changeTime = function(ele, time, endfn){
+        var intv = setInterval(function(){
+            if(time == 1){
+                clearInterval(intv);
+                endfn.call(this,ele);
+                return;
+            }
+            time--;
+            ele.text(time);
+        },1000)
     },
     initScreen = function(){
         var win_width = document.documentElement.clientWidth,
@@ -330,32 +198,6 @@ var Index = function(){
         setTimeout(function(){
             ele.hide();
         },300)
-    },    
-    addMess = function(txt){
-        var ele_p = document.createElement('p');
-        ele_p.innerHTML ='<span class="s-icon '+ _imgClass +'"></span>'
-                        +    '<span class="s-txt">'+ txt +'</span>';
-        $messwin.append(ele_p);
-    },
-    addAudio = function(url,time){
-        setTimeout(function(){
-            var p_html = '<p>'
-                        +    '<span class="s-icon '+ _imgClass +'"></span>'
-                        +    '<span class="s-yuyin"><em></em></span>'
-                        +    '<span class="s-time"> '+ time +'"</span>'
-                        +'</p>'; 
-            $messwin.append(p_html);
-            _messaudio.play();
-            $audio.attr('src', __CDNPATH+url);
-            $messwin.find('.s-yuyin').bind('touchend',function(){
-                if(_isPlaying){return};
-                var $this = $(this);
-                $this.addClass('play');
-                $audio.get(0).play();
-                audioIsOver($audio.get(0));
-                _isPlaying = true;
-            })
-        },1500)
     },
     audioIsOver = function(ele){
         clearTimeout(_audioTimeout);
@@ -371,57 +213,19 @@ var Index = function(){
             },3000)
         }, false);
     },
+    playVideo = function(){
+        $stages.eq(1).hide();
+        $stages.eq(2).hide();
+        showEle($stages.eq(3));
+        var vid = $('#Jvideo')[0];
+        vid.play();
+        vid.addEventListener("ended",function(evt) {
+            jumpLastStage();
+       });
+    },
     jumpLastStage = function(){
-        var $tool = $stages.eq(3).find('.m-messinput-tool');
-        $tool.unbind('touchend');
-        $tool.bind('touchend',function(){
-            $stages.eq(3).hide();
-            showEle($stages.eq(4));
-        })
-    },
-    choseMess = function(){
-        var $mess_P = $stages.eq(3).find('.m-messinput p');
-        $mess_P.eq(0).html(_dataArr[_chose][_messName]['txt1'][0]);
-        $mess_P.eq(1).html(_dataArr[_chose][_messName]['txt1'][1]);
-        $mess_P.bind('touchend',function(){
-            var $this = $(this);
-            $mess_P.removeClass('active');
-            $this.addClass('active');
-            $messinputTxt.html($this.text());
-        })
-    },
-    audioInit = function(){
-        var _this = this,
-            hasTouch = 'ontouchstart' in window,
-            startEvent = hasTouch ? 'touchstart' : 'mousedown',
-            moveEvent = hasTouch ? 'touchmove' : 'mousemove',
-            endEvent = hasTouch ? 'touchend' : 'mouseup',
-            cancelEvent = hasTouch ? 'touchcancel' : 'mouseup',            
-            isPlay = false;
-
-        $('body').bind(startEvent,function(e){
-            if(e.target.id == 'Jplaybtn'){
-                _audio.pause();
-                $audio_btn.removeClass('on');
-                isPlay = false;
-                _audio.setAttribute('src','');
-            }else if(!isPlay){
-                $audio_btn.addClass('on');
-                _audio.play();
-                isPlay = true;
-            }            
-        })
-
-        $audio_btn.bind(startEvent, function() {
-            var $this = $(this);
-            if(!_audio.paused){
-                _audio.pause();
-                $this.removeClass('on');
-            }else{
-                _audio.play();
-                $this.addClass('on');
-            }
-        });
+        $stages.eq(3).hide();
+        showEle($stages.eq(4));
     },
     randomNum = function(n,m){
         var c = m - n + 1;
@@ -534,20 +338,105 @@ var Index = function(){
             
         }
     },
-    shareFun = function(){
-        nie.use(["nie.util.mobiShare"],function(){
-            $(function(){
-                MobileShare.setting({//设置分享文案
-                    title: shareInfo.title,//分享标题
-                    desc: shareInfo.desc,//分享正文
-                    url: shareInfo.url,//分享URL
-                    imgurl: shareInfo.imgurl,//分享图片
-                    callback:function(o){
-                        //o.type：-1是分享失败，0是取消分享，1是分享到朋友圈，2是发送给好友
-                    }//分享后的回调
-                 })
+    shareSDK = {
+        _info: {
+            shareTitle: $('#share_title').html(),
+            descContent: $('#share_desc').html(),
+            shareTimeTitle: '',
+            imgUrl: $('#share_pic').attr('data-src'),
+            lineLink: window.location.href
+        },
+
+        _doneCbk: null,
+
+        _doneShareFriend: function() {
+            // nie.config.stats.url.add('1/targetname.html?click=share', '分享给好友');
+            this._doneCbk && this._doneCbk();
+        },
+
+        _doneShareTimeline: function() {
+            // nie.config.stats.url.add('1/targetname.html?click=share', '分享到朋友圈');
+            this._doneCbk && this._doneCbk();
+        },
+
+        set: function(info) {
+
+            var that = this;
+
+            if (info) {
+                info.shareTitle && (this._info.shareTitle = info.shareTitle);
+                info.descContent && (this._info.descContent = info.descContent);
+                info.shareTimeTitle && (this._info.shareTimeTitle = info.shareTimeTitle);
+                info.imgUrl && (this._info.imgUrl = info.imgUrl);
+                info.lineLink && (this._info.lineLink = info.lineLink);
+                info.doneCbk && (this._doneCbk = info.doneCbk);
+            }
+
+            wx.ready(function() {
+                wx.onMenuShareAppMessage({
+                    title: that._info.shareTitle,
+                    desc: that._info.descContent,
+                    link: that._info.lineLink,
+                    imgUrl: that._info.imgUrl,
+                    success: function() {
+                        that._doneShareFriend();
+                    }
+                });
+                wx.onMenuShareTimeline({
+                    title: that._info.shareTimeTitle,
+                    link: that._info.lineLink,
+                    imgUrl: that._info.imgUrl,
+                    success: function() {
+                        that._doneShareTimeline();
+                    }
+                });
             });
-        });
+
+            return this;
+        },
+
+        init: function() {
+
+            var that = this;
+
+            document.addEventListener('YixinJSBridgeReady', function() {
+                YixinJSBridge.on('menu:share:appmessage', function() {
+                    YixinJSBridge.invoke('sendAppMessage', {
+                        img_width: '300',
+                        img_height: '300',
+                        img_url: that._info.imgUrl,
+                        link: that._info.lineLink,
+                        desc: that._info.descContent,
+                        title: that._info.shareTitle
+                    }, function() {
+                        that._doneShareFriend();
+                    });
+                });
+                YixinJSBridge.on('menu:share:timeline', function() {
+                    YixinJSBridge.invoke('shareTimeline', {
+                        img_width: '300',
+                        img_height: '300',
+                        img_url: that._info.imgUrl,
+                        link: that._info.lineLink,
+                        desc: that._info.shareTimeTitle,
+                        title: that._info.shareTimeTitle
+                    }, function() {
+                        that._doneShareTimeline();
+                    });
+                });
+            }, false);
+
+            wx_conf.jsApiList = [
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage'
+            ];
+
+            wx.config(wx_conf);
+
+            this.set();
+
+            return this;
+        }
     };
     return{
         init: init
